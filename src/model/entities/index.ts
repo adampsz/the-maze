59b7b9e8464474210d41d6996832d20fe52ff4b1
Adam Szeruda <32493319+adampsz@users.noreply.github.com>
2021-07-03 11:29:23 +0200

    Remove unused NeutralEntity

--- src/model/entities/index.ts
+++ src/model/entities/index.ts
@@ -1,6 +1,5 @@
 export { default as Entity } from "./Entity";
 export { default as HostileEntity } from "./HostileEntity";
-export { default as NeutralEntity } from "./NeutralEntity";
 export { default as Monster } from "./Monster";
 export { default as Thief } from "./Thief";
 export { default as Archer } from "./Archer";
